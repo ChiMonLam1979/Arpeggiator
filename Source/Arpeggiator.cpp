a6e59806bb50c3e3d71097db05fa8135ebec4327
ChiMon <Chi-Mon.NoelLam@komplett.com>
2019-04-28 02:39:12 +0200

    [minor] refactored and split into .h and .cpp

---
+++ Source/Arpeggiator.cpp
@@ -0,0 +1,162 @@
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "Arpeggiator.h"
+
+Arpeggiator::Arpeggiator(): AudioProcessor(BusesProperties().withInput("Input", AudioChannelSet::stereo(), true))
+{
+	addParameter(noteDivision = new AudioParameterChoice("divisions", "Note Divisions", { "1/4 note", "1/16 note", "1/32 note" }, 0));
+	addParameter(lengthFactor = new AudioParameterFloat("length", "Note Length", 0.1f, 0.9f, 0.5f));
+}
+
+void Arpeggiator::prepareToPlay(double sampleRate, int)
+{
+	notesToPlay.clear();
+	rate = sampleRate;
+	currentNoteIndex = 0;
+	lastNoteValue = -1;
+	noteDivisionFactor = 1;
+	samplesFromLastNoteOnUntilBufferEnds = 0;
+	noteLengthInSamples = 0;
+	numberOfSamplesInBuffer = 0;
+	lastNoteWasNoteOn = false;
+	noteDivisionFactorChanged = false;
+	noteOffRequiredThisBuffer = false;
+}
+
+void Arpeggiator::processBlock(AudioBuffer<float>& buffer, MidiBuffer& midiMessages)
+{
+	auto playHead = getPlayHead();
+	if (playHead == nullptr) return;
+
+	AudioPlayHead::CurrentPositionInfo positionInfo{};
+	playHead->getCurrentPosition(positionInfo);
+
+	const auto quarterNotesPerMinute = positionInfo.bpm;
+	const auto quarterNotesPerSecond = quarterNotesPerMinute / 60;
+	const auto samplesPerQuarterNote = rate / quarterNotesPerSecond;
+	const auto samplesPerNoteDivision = samplesPerQuarterNote / noteDivisionFactor;
+	noteLengthInSamples = static_cast<int> (std::ceil(samplesPerNoteDivision * *lengthFactor));
+	numberOfSamplesInBuffer = buffer.getNumSamples();
+
+	const double partsPerQuarterNoteStartPosition = positionInfo.ppqPosition;
+	const double NoteDivisionStartPosition = partsPerQuarterNoteStartPosition * noteDivisionFactor;
+	const double NoteDivisionEndPosition = NoteDivisionStartPosition + (numberOfSamplesInBuffer / samplesPerNoteDivision);
+	// trick to calculate when a new note should occur..
+	const int NoteDivisionStartPositionAsInt = std::ceil(NoteDivisionStartPosition);
+	const int NoteDivisionEndPositionAsInt = std::floor(NoteDivisionEndPosition);
+
+	MidiMessage message;
+	int ignore;
+
+	for (MidiBuffer::Iterator it(midiMessages); it.getNextEvent(message, ignore);)
+	{
+		if (message.isNoteOn())
+		{
+			notesToPlay.add(message.getNoteNumber());
+		}
+		else if (message.isNoteOff())
+		{
+			notesToPlay.removeValue(message.getNoteNumber());
+		}
+	}
+
+	midiMessages.clear();
+
+	UpdateNoteDivision();
+
+	if (positionInfo.isPlaying)
+	{
+		if ((NoteOffIsRequiredThisBuffer() || noteDivisionFactorChanged) && lastNoteWasNoteOn)
+		{
+			isSameBufferAsLastNoteOn = false;
+			const auto offsetForNoteOff = CalculateOffsetForNoteOff();
+			AddNoteOffToBuffer(midiMessages, offsetForNoteOff);
+		}
+
+		for (int i = NoteDivisionStartPositionAsInt; i <= NoteDivisionEndPositionAsInt; ++i)
+		{
+			const int noteOnOffset = (int)samplesPerNoteDivision * (i - NoteDivisionStartPosition);
+
+			if (AnyNotesToPlay())
+			{
+				UpdateNoteValue();
+				AddNoteOnToBuffer(midiMessages, noteOnOffset);
+				samplesFromLastNoteOnUntilBufferEnds = numberOfSamplesInBuffer - noteOnOffset;
+			}
+
+			if (NoteOffIsRequiredThisBuffer())
+			{
+				isSameBufferAsLastNoteOn = true;
+				const auto offsetForNoteOff = CalculateOffsetForNoteOff(noteOnOffset);
+				AddNoteOffToBuffer(midiMessages, offsetForNoteOff);
+			}
+		}
+
+		samplesFromLastNoteOnUntilBufferEnds = (samplesFromLastNoteOnUntilBufferEnds + numberOfSamplesInBuffer);
+	}
+}
+
+void Arpeggiator::UpdateNoteDivision()
+{
+	const auto currentNoteDivisionFactor = noteDivisionDictionary[noteDivision->getCurrentChoiceName()];
+
+	noteDivisionFactorChanged = noteDivisionFactor != currentNoteDivisionFactor;
+	if (noteDivisionFactorChanged)
+	{
+		noteDivisionFactor = currentNoteDivisionFactor;
+	}
+}
+
+void Arpeggiator::AddNoteOffToBuffer(MidiBuffer& midiMessages, const int offset)
+{
+	midiMessages.addEvent(MidiMessage::noteOff(1, lastNoteValue), offset);
+	lastNoteWasNoteOn = false;
+}
+
+void Arpeggiator::AddNoteOnToBuffer(MidiBuffer& midiMessages, const int offset)
+{
+	midiMessages.addEvent(MidiMessage::noteOn(1, lastNoteValue, (uint8)127), offset);
+	lastNoteWasNoteOn = true;
+}
+
+void Arpeggiator::UpdateNoteValue()
+{
+	currentNoteIndex = (currentNoteIndex + 1) % notesToPlay.size();
+	lastNoteValue = notesToPlay[currentNoteIndex];
+}
+
+bool Arpeggiator::NoteOffIsRequiredThisBuffer() const
+{
+	return samplesFromLastNoteOnUntilBufferEnds >= noteLengthInSamples;
+}
+
+int Arpeggiator::CalculateOffsetForNoteOff(int noteOnOffset) const
+{
+	const auto startOfLastSampleInBuffer = numberOfSamplesInBuffer - 1;
+	if(isSameBufferAsLastNoteOn)
+	{
+		const auto offset = noteOnOffset + noteLengthInSamples;
+		return jmin(offset, startOfLastSampleInBuffer);
+	}
+	else
+	{
+		const auto offset = numberOfSamplesInBuffer - (samplesFromLastNoteOnUntilBufferEnds - noteLengthInSamples);
+		return jmin(offset, startOfLastSampleInBuffer);
+	}
+}
+
+bool Arpeggiator::AnyNotesToPlay() const
+{
+	return notesToPlay.size() > 0;
+}
+
+void Arpeggiator::getStateInformation(MemoryBlock& destData)
+{
+	MemoryOutputStream(destData, true).writeInt(*noteDivision);
+	MemoryOutputStream(destData, true).writeFloat(*lengthFactor);
+}
+
+void Arpeggiator::setStateInformation(const void* data, int sizeInBytes)
+{
+	noteDivision->setValueNotifyingHost(MemoryInputStream(data, static_cast<size_t> (sizeInBytes), false).readFloat());
+	lengthFactor->setValueNotifyingHost(MemoryInputStream(data, static_cast<size_t> (sizeInBytes), false).readFloat());
+}
--- Source/Arpeggiator.h
+++ Source/Arpeggiator.h
@@ -4,118 +4,12 @@ class Arpeggiator : public AudioProcessor
 {
 public:
 
-	Arpeggiator()
-		: AudioProcessor(BusesProperties()
-			.withInput("Input", AudioChannelSet::stereo(), true)
-		)
-	{
-		addParameter(noteDivision = new AudioParameterChoice("divisions", "Note Divisions", { "1/4 note", "1/16 note", "1/32 note" }, 0));
-		addParameter(lengthFactor = new AudioParameterFloat("length", "Note Length", 0.1, 0.9, 0.5));
-	}
-
+	Arpeggiator();
 	~Arpeggiator() {}
 
-	void prepareToPlay(double sampleRate, int) override
-	{
-		notes.clear();
-		currentNoteIndex = 0;
-		lastNoteValue = -1;
-		lastNoteWasNoteOn = false;
-		rate = sampleRate;
-		samplesUntilBufferEnds = 0;
-		noteDivisionFactor = 1;
-		noteDivisionFactorChanged = false;
-		noteOffRequiredThisBuffer = false;
-	}
-
+	void prepareToPlay(double sampleRate, int) override;
 	void releaseResources() override {}
-
-	void processBlock(AudioBuffer<float>& buffer, MidiBuffer& midiMessages) override
-	{
-		AudioPlayHead* playHead = getPlayHead();
-		if (playHead == nullptr) return;
-
-		AudioPlayHead::CurrentPositionInfo positionInfo;
-		playHead->getCurrentPosition(positionInfo);
-
-		const auto beatsPerMinute = positionInfo.bpm;																// quarterNotesPerMinute
-		const auto beatsPerSecond = beatsPerMinute / 60;															// quarterNotesPerSecond
-		const auto samplesPerBeat = rate / beatsPerSecond;															// number of samples per beat/quarternote is samples per sec / beats per second
-		const auto samplesPerNoteDivision = samplesPerBeat / noteDivisionFactor;									// set note division
-		const auto noteLengthInSamples = static_cast<int> (std::ceil(samplesPerNoteDivision * *lengthFactor));		// set note length
-		const auto numberOfSamplesInBuffer = buffer.getNumSamples();
-
-		//work out which notes will occur in the buffer
-		const double partsPerQuarterNoteStart = positionInfo.ppqPosition;
-		const double partsPerNoteDivisionStart = partsPerQuarterNoteStart * noteDivisionFactor;
-		const double partsPerNoteDivisionEnd = partsPerNoteDivisionStart + (numberOfSamplesInBuffer / samplesPerNoteDivision);
-		const int partsPerNoteDivisionStartAsInt = std::ceil(partsPerNoteDivisionStart);
-		const int partsPerNoteDivisionEndAsInt = std::floor(partsPerNoteDivisionEnd);
-
-		MidiMessage message;
-		int ignore;
-
-		for (MidiBuffer::Iterator it(midiMessages); it.getNextEvent(message, ignore);)
-		{
-			if (message.isNoteOn())
-			{
-				notes.add(message.getNoteNumber());
-			}
-			else if (message.isNoteOff())
-			{
-				notes.removeValue(message.getNoteNumber());
-			}
-		}
-
-		midiMessages.clear();
-
-		auto currentNoteDivisionFactor = noteDivisionDictionary[noteDivision->getCurrentChoiceName()];
-
-		noteDivisionFactorChanged = noteDivisionFactor != currentNoteDivisionFactor;
-		if (noteDivisionFactorChanged)
-		{
-			noteDivisionFactor = currentNoteDivisionFactor;
-		}
-
-		if (positionInfo.isPlaying)
-		{
-			noteOffRequiredThisBuffer = samplesUntilBufferEnds >= noteLengthInSamples;
-			if ((noteOffRequiredThisBuffer || noteDivisionFactorChanged) && lastNoteWasNoteOn)
-			{
-				auto offsetForNoteOff = jmin((numberOfSamplesInBuffer - (samplesUntilBufferEnds - noteLengthInSamples)), numberOfSamplesInBuffer - 1);
-				midiMessages.addEvent(MidiMessage::noteOff(1, lastNoteValue), offsetForNoteOff);
-				lastNoteWasNoteOn = false;
-			}
-
-			// for each note transition in the buffer...
-			for (int i = partsPerNoteDivisionStartAsInt; i <= partsPerNoteDivisionEndAsInt; ++i)
-			{
-				//work out the exact sample where the note occurs
-				const int offset = (int)samplesPerNoteDivision * (i - partsPerNoteDivisionStart);
-
-				if (notes.size() > 0)
-				{
-					currentNoteIndex = (currentNoteIndex + 1) % notes.size();
-					lastNoteValue = notes[currentNoteIndex];
-					midiMessages.addEvent(MidiMessage::noteOn(1, lastNoteValue, (uint8)127), offset);
-
-					lastNoteWasNoteOn = true;
-					samplesUntilBufferEnds = numberOfSamplesInBuffer - offset;
-				}
-
-				noteOffRequiredThisBuffer = samplesUntilBufferEnds >= noteLengthInSamples;
-				if (noteOffRequiredThisBuffer)
-				{
-					const auto OffsetForNoteOff = jmin((offset + noteLengthInSamples), numberOfSamplesInBuffer - 1);
-					midiMessages.addEvent(MidiMessage::noteOff(1, lastNoteValue), OffsetForNoteOff);
-					lastNoteWasNoteOn = false;
-				}
-			}
-
-			samplesUntilBufferEnds = (samplesUntilBufferEnds + numberOfSamplesInBuffer);	// add next buffer size
-		}
-	}
-
+	void processBlock(AudioBuffer<float>& buffer, MidiBuffer& midiMessages) override;
 	bool isMidiEffect() const override { return true; }
 
 	AudioProcessorEditor* createEditor() override { return new GenericAudioProcessorEditor(this); }
@@ -133,17 +27,8 @@ public:
 	const String getProgramName(int) override { return {}; }
 	void changeProgramName(int, const String&) override {}
 
-	void getStateInformation(MemoryBlock& destData) override
-	{
-		MemoryOutputStream(destData, true).writeFloat(*noteDivision);
-		MemoryOutputStream(destData, true).writeFloat(*lengthFactor);
-	}
-
-	void setStateInformation(const void* data, int sizeInBytes) override
-	{
-		noteDivision->setValueNotifyingHost(MemoryInputStream(data, static_cast<size_t> (sizeInBytes), false).readFloat());
-		lengthFactor->setValueNotifyingHost(MemoryInputStream(data, static_cast<size_t> (sizeInBytes), false).readFloat());
-	}
+	void getStateInformation(MemoryBlock& destData) override;
+	void setStateInformation(const void* data, int sizeInBytes) override;
 
 private:
 
@@ -153,12 +38,23 @@ private:
 	bool noteDivisionFactorChanged;
 	bool lastNoteWasNoteOn;
 	bool noteOffRequiredThisBuffer;
+	bool isSameBufferAsLastNoteOn;
 	int currentNoteIndex;
 	int lastNoteValue;
-	int samplesUntilBufferEnds;
+	int noteLengthInSamples;
+	int samplesFromLastNoteOnUntilBufferEnds;
+	int numberOfSamplesInBuffer;
 	double rate;
-	SortedSet<int> notes;
+	SortedSet<int> notesToPlay;
 	std::map<juce::String, int> noteDivisionDictionary = { {"1/4 note", 1}, {"1/16 note", 4}, {"1/32 note", 8} };
+
+	void UpdateNoteDivision();
+	void AddNoteOffToBuffer(MidiBuffer& midiMessages, const int offset);
+	void AddNoteOnToBuffer(MidiBuffer& midiMessages, const int offset);
+	void UpdateNoteValue();
+	bool AnyNotesToPlay() const;
+	bool NoteOffIsRequiredThisBuffer() const;
+	int CalculateOffsetForNoteOff(int noteOnOffset = 0) const;
 
 	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Arpeggiator)
 };
